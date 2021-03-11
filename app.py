4a112d517ebb6c26e319ed610f12ee5f2f89961c
Anton Huck <antonhuck@gmail.com>
2021-03-11 17:48:13 +0100

    Use .get() for safer dict access

--- app.py
+++ app.py
@@ -33,9 +33,8 @@ def read():
         resp.set_data(snappy.compress(read_response.SerializeToString()))
         return resp
 
-    if res['totalCount'] > 0:
-        for dt_result in res['result']:
-            add_result(query_result, dt_result)
+    for dt_result in res.get('result', []):
+        add_result(query_result, dt_result)
 
     resp.set_data(snappy.compress(read_response.SerializeToString()))
     
@@ -68,28 +67,35 @@ def write():
 
 def get_entity(dimension):
     url = f"https://{app.config['DT_TENANT']}/api/v2/entities/{dimension}"
-    headers = {'Authorization': f"Api-Token {app.config['DT_API_TOKEN']}"}
+    headers = {
+        'Authorization': f"Api-Token {app.config['DT_API_TOKEN']}"
+    }
 
-    x = requests.get(url, headers = headers)
+    resp = requests.get(url, headers = headers)
 
-    return(x.json()['displayName'])
+    return resp.json().get('displayName', dimension)
 
 def ingest_metric(content):
     url = f"https://{app.config['DT_TENANT']}/api/v2/metrics/ingest"
-    headers = {'Authorization': f"Api-Token {app.config['DT_API_TOKEN']}", 'Content-Type': 'text/plain'}
+    headers = {
+        'Authorization': f"Api-Token {app.config['DT_API_TOKEN']}", 
+        'Content-Type': 'text/plain'
+    }
 
     requests.post(url, headers = headers, data = content)
 
 def query_metric(metric, from_ts, to_ts):
     url = (
-        f"https://{app.config['DT_TENANT']}/api/v2/metrics/query?" \
-        f"metricSelector={metric}&" \
-        f"from={from_ts}&" \
-        f"to={to_ts}"
+        f"https://{app.config['DT_TENANT']}/api/v2/metrics/query"
     )
+    params = {
+        "metricSelector": metric,
+        "from": from_ts,
+        "to": to_ts
+    }
     headers = {'Authorization': f"Api-Token {app.config['DT_API_TOKEN']}"}
-
-    x = requests.get(url, headers = headers)
+    
+    x = requests.get(url, params = params, headers = headers)
     return(x.json())
 
 def add_result(query_result, result):
@@ -99,14 +105,14 @@ def add_result(query_result, result):
         timeseries = query_result.timeseries.add()
         timeseries.labels.add(name="__name__", value=metric_name)
 
-        for dimension in data['dimensions']:
+        for dimension in data.get('dimensions', []):
             if dimension not in dim_map:
                 dim_map[dimension] = get_entity(dimension)
 
-        for dimension_name, dimension_value in data['dimensionMap'].items():
+        for dimension_name, dimension_value in data.get('dimensionMap', {}).items():
             timeseries.labels.add(name=re.sub(r'[^a-z0-9]', "_", dimension_name.lower()), value=dim_map[dimension_value])
         
-        for i, timestamp in enumerate(data['timestamps']):
+        for i, timestamp in enumerate(data.get('timestamps', [])):
             value = data['values'][i]
             if value != None:
                 timeseries.samples.add(value = value, timestamp = timestamp)
