3d7cd548ec3a9964d97f8a7282d6455c7d3cfe23
Anton Huck <antonhuck@gmail.com>
2021-03-17 16:51:21 +0100

    Extract clients and implement two-way lookup

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import snappy
 from flask import Flask, Response, request
 from flask.logging import create_logger
 from proto import remote_pb2
-
+from dynatrace.entity_client import CachedEntityClient
+from dynatrace.metrics_client import CachedMetricsClient
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -14,6 +15,9 @@ log = create_logger(app)
 
 dim_map = {}
 dimension_name_bidict = bidict()
+
+entity_client = CachedEntityClient(app.config['DT_TENANT'], app.config['DT_API_TOKEN'])
+metrics_client = CachedMetricsClient(app.config['DT_TENANT'], app.config['DT_API_TOKEN'])
 
 @app.route('/read', methods=["POST"])
 def read():
@@ -26,15 +30,22 @@ def read():
 
     query = msg.queries[0]
     metric_name = ''
-    matchers = []
     for matcher in query.matchers:
         if matcher.name == '__name__':
-            metric_name = matcher.value
-        else:
-            dt_matcher_name = dimension_name_bidict.inverse.get(matcher.name, matcher.name)
-            matchers.append(f"eq({dt_matcher_name},{matcher.value})")
+            metric_name = metrics_client.get_dt_metric_from_prom(matcher.value)
 
-    res = query_metric(metric_name, query.start_timestamp_ms, query.end_timestamp_ms, matchers)
+    metrics_client.prefetch_dimensions(metric_name)
+
+    matchers = []
+    for matcher in query.matchers:
+        if matcher.name != '__name__':
+            dt_matcher_name = metrics_client.get_dimension_from_label(matcher.name)
+            entity_client.prefetch_entities(dt_matcher_name)
+
+            dt_matcher_value = entity_client.get_entity_id(matcher.value)
+            matchers.append(f"eq({dt_matcher_name},{dt_matcher_value})")
+
+    res = metrics_client.query_metric(metric_name, query.start_timestamp_ms, query.end_timestamp_ms, matchers)
 
     read_response = remote_pb2.ReadResponse()
     query_result = read_response.results.add()
@@ -72,46 +83,9 @@ def write():
             if str(sample.value) != "nan":
                 to_send.append(f"{dt} {str(sample.value)} {str(sample.timestamp)}")
 
-    ingest_metric("\n".join(to_send))
+    metrics_client.ingest_metric("\n".join(to_send))
 
     return 'OK'
-
-def get_entity(dimension):
-    url = f"https://{app.config['DT_TENANT']}/api/v2/entities/{dimension}"
-    headers = {
-        'Authorization': f"Api-Token {app.config['DT_API_TOKEN']}"
-    }
-
-    resp = requests.get(url, headers = headers)
-
-    return resp.json().get('displayName', dimension)
-
-def ingest_metric(content):
-    url = f"https://{app.config['DT_TENANT']}/api/v2/metrics/ingest"
-    headers = {
-        'Authorization': f"Api-Token {app.config['DT_API_TOKEN']}", 
-        'Content-Type': 'text/plain'
-    }
-
-    requests.post(url, headers = headers, data = content)
-
-def query_metric(metric, from_ts, to_ts, matchers):
-    url = (
-        f"https://{app.config['DT_TENANT']}/api/v2/metrics/query"
-    )
-    params = {
-        "metricSelector": metric,
-        "from": from_ts,
-        "to": to_ts
-    }
-
-    if len(matchers) > 0:
-        params["metricSelector"] += f":filter(or({','.join(matchers)}))"
-
-    headers = {'Authorization': f"Api-Token {app.config['DT_API_TOKEN']}"}
-    
-    x = requests.get(url, params = params, headers = headers)
-    return(x.json())
 
 def add_result(query_result, result):
     metric_name_elements = re.sub(r'[^a-z0-9:]', "_", result.get('metricId','').lower()).split(":")
@@ -126,12 +100,10 @@ def add_result(query_result, result):
 
         for dimension in data.get('dimensions', []):
             if dimension not in dim_map:
-                dim_map[dimension] = get_entity(dimension)
+                dim_map[dimension] = entity_client.get_entity_name(dimension)
 
         for dimension_name, dimension_value in data.get('dimensionMap', {}).items():
-            prom_dim_name = re.sub(r'[^a-z0-9]', "_", dimension_name.lower())
-            dimension_name_bidict.forceput(dimension_name, prom_dim_name)
-            timeseries.labels.add(name=prom_dim_name, value=dim_map[dimension_value])
+            timeseries.labels.add(name=metrics_client.get_label_from_dimension(dimension_name), value=dim_map[dimension_value])
 
         for i, timestamp in enumerate(data.get('timestamps', [])):
             value = data['values'][i]
