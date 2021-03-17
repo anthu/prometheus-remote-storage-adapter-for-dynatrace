7285cc1a1526c6f726926b16782ecd4b7a55c0fc
Anton Huck <antonhuck@gmail.com>
2021-03-17 11:58:32 +0100

    Use bidict for dimension lookup
    You'll need to query the api without the filter to get the bidict to be filled up with the values. Working on a workaround for this.

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import re
+from bidict import bidict
 import requests
 import snappy
 from flask import Flask, Response, request
+from flask.logging import create_logger
 import remote_pb2
 
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
+log = create_logger(app)
+
 dim_map = {}
+dimension_name_bidict = bidict()
 
 @app.route('/read', methods=["POST"])
 def read():
@@ -18,22 +23,24 @@ def read():
     resp = Response()
     resp.headers['Content-Type'] = 'application/x-protobuf'
     resp.headers['Content-Encoding'] = 'snappy'
-    
+
     query = msg.queries[0]
     metric_name = ''
     matchers = []
     for matcher in query.matchers:
         if matcher.name == '__name__':
-            metric = matcher.value
+            metric_name = matcher.value
         else:
-            matchers.append(f"eq({matcher.name},{matcher.value})")
+            dt_matcher_name = dimension_name_bidict.inverse.get(matcher.name, matcher.name)
+            matchers.append(f"eq({dt_matcher_name},{matcher.value})")
+
     res = query_metric(metric_name, query.start_timestamp_ms, query.end_timestamp_ms, matchers)
 
     read_response = remote_pb2.ReadResponse()
     query_result = read_response.results.add()
 
     if 'error' in res:
-        app.logger.error(f"Dytrace returned an error: {res['error']}")
+        log.error("Dytrace returned an error: %s", res['error'])
         resp.set_data(snappy.compress(read_response.SerializeToString()))
         return resp
 
@@ -41,7 +48,7 @@ def read():
         add_result(query_result, dt_result)
 
     resp.set_data(snappy.compress(read_response.SerializeToString()))
-    
+
     return resp
 
 @app.route('/write', methods=["POST"])
@@ -51,7 +58,7 @@ def write():
 
     to_send = []
     for timeseries in msg.timeseries:
-        
+
         dt_dimensions = []
         for label in timeseries.labels:
             if label.name == '__name__':
@@ -88,7 +95,7 @@ def ingest_metric(content):
 
     requests.post(url, headers = headers, data = content)
 
-def query_metric(metric, from_ts, to_ts, matchers=[]):
+def query_metric(metric, from_ts, to_ts, matchers):
     url = (
         f"https://{app.config['DT_TENANT']}/api/v2/metrics/query"
     )
@@ -107,8 +114,12 @@ def query_metric(metric, from_ts, to_ts, matchers=[]):
     return(x.json())
 
 def add_result(query_result, result):
-    metric_name = re.sub(r'[^a-z0-9:]', "_", result.get('metricId','').lower())
-    
+    metric_name_elements = re.sub(r'[^a-z0-9:]', "_", result.get('metricId','').lower()).split(":")
+
+    metric_name = metric_name_elements[0]
+    if metric_name_elements[0] == 'builtin' or metric_name_elements[0] == 'ext':
+        metric_name += f":{metric_name_elements[1]}"
+
     for data in result['data']:
         timeseries = query_result.timeseries.add()
         timeseries.labels.add(name="__name__", value=metric_name)
@@ -118,11 +129,13 @@ def add_result(query_result, result):
                 dim_map[dimension] = get_entity(dimension)
 
         for dimension_name, dimension_value in data.get('dimensionMap', {}).items():
-            timeseries.labels.add(name=re.sub(r'[^a-z0-9]', "_", dimension_name.lower()), value=dim_map[dimension_value])
-        
+            prom_dim_name = re.sub(r'[^a-z0-9]', "_", dimension_name.lower())
+            dimension_name_bidict.forceput(dimension_name, prom_dim_name)
+            timeseries.labels.add(name=prom_dim_name, value=dim_map[dimension_value])
+
         for i, timestamp in enumerate(data.get('timestamps', [])):
             value = data['values'][i]
-            if value != None:
+            if value is not None:
                 timeseries.samples.add(value = value, timestamp = timestamp)
 
 if __name__ == "__main__":
